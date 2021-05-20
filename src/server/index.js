b3f42d6f3cd783282584f91d6a35829b1c926c9e
Dominic <dominicvana@gmail.com>
2021-05-20 09:00:27 +0100

    Refactor: separate function for createQuote route

--- src/server/index.js
+++ src/server/index.js
@@ -5,24 +5,26 @@ const PORT = 3000;
 
 const router = function (request, response) {
   const { method, url, headers } = request;
+
+  if (method == 'POST') {
+    createQuote(request, response);
+  }
+};
+
+const createQuote = function (request, response) {
   let body = [];
   request
     .on('error', (err) => console.error(err))
     .on('data', (chunk) => body.push(chunk))
     .on('end', () => {
       body = Buffer.concat(body).toString();
+      console.log(body);
 
       response.on('error', (err) => console.error(err));
     });
 
   response.statusCode = 200;
-  // response.setHeader('Content-Type', 'application/json');
-
-  // const responseBody = { headers, method, url, body };
-
-  // response.write(JSON.stringify(responseBody));
   response.end();
-  // response.end(JSON.stringify(responseBody)) produces same result
 };
 
 const server = http.createServer(router);
